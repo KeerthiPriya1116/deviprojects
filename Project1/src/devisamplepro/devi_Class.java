cb329c7cbc24bc1857a3ca3b798703d149fa2dd2
LOKI <LOKI@DEVI>
2024-07-28 20:10:48 +0530

    added deviproject1

---
+++ Project1/src/devisamplepro/devi_Class.java
@@ -0,0 +1,10 @@
+package devisamplepro;
+
+public class devi_Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("hii..i am devi");
+	}
+
+}
