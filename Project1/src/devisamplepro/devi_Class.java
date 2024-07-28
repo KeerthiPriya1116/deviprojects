48fa5bbcd0b5904cc9ca96f0e56b8beb0e5361ce
LOKI <LOKI@DEVI>
2024-07-28 20:23:00 +0530

    updated deviclass

--- Project1/src/devisamplepro/devi_Class.java
+++ Project1/src/devisamplepro/devi_Class.java
@@ -5,6 +5,7 @@ public class devi_Class {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("hii..i am devi");
+       System.out.println("hello...How are you");
 	}
 
 }
