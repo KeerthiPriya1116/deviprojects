c3998cdc0f21f380baca2c0a3ba92e22c6f21aec
pkeer <pkeer@DESKTOP-BUK2US3.mshome.net>
2024-07-29 10:38:00 +0530

    added class2

---
+++ Project1/src/devisamplepro/Dev_iclass2.java
@@ -0,0 +1,10 @@
+package devisamplepro;
+
+public class Dev_iclass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Hii...Gooo");
+	}
+
+}
