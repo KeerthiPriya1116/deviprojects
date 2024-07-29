e9ca59c345a0a26ea0b2e367ac1575ef3f6dccf4
pkeer <pkeer@DESKTOP-BUK2US3.mshome.net>
2024-07-29 10:24:47 +0530

    added class2

---
+++ Project1/src/devisamplepro/class2.java
@@ -0,0 +1,10 @@
+package devisamplepro;
+
+public class class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Helloo...I am class2");
+	}
+
+}
